2b4993f1838eae58617e629da41cde22138a8b5a
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-02-05 21:49:01 +0000

    create seeds for all tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO users VALUES (1, 'Matthew Chan', 'm.zj.chan@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users VALUES (2, 'Ina Ina', 'Ina@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users VALUES (3, 'Lean Lean', 'lean@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties VALUES (1, 1, 'Centro Condo', 'penthouse in Richmond.', 'url', 'url', 199, 3, 3, 3, 'Canada', 'no. 3 rd.', 'Richmond', 'BC', 'V6Y 0B5', true);
+INSERT INTO properties VALUES (2, 3, 'Vancouver Basement', 'Basement in Vancouver.', 'url', 'url', 99, 1, 1, 1, 'CA', '6th street', 'BC', 'van', 'V7Y 9K6');
+INSERT INTO properties VALUES (3, 2, 'Surrey Room', 'bedroom in Surrey.', 'url', 'url', 49, 0, 1, 1, 'CA', 'Granville', 'BC','Surrey', 'T3A 3P7');
+
+
+INSERT INTO reservations VALUES (1, 'Jan 05, 2020', 'Jan 07, 2020', 1, 3);
+INSERT INTO reservations VALUES (2, 'Apr 10, 2020', 'Apr 30, 2020', 2, 1);
+INSERT INTO reservations VALUES (3, 'Feb 16, 2020', 'Feb 19, 2020', 3, 2);
+
+
+INSERT INTO property_reviews VALUES (1, 1, 3, 1, 5, 'not bad'), (2, 2, 1, 2, 3, 'Its ggggggggreat'), (3, 2, 1, 3, 1, 'garbage');
+
+
+
+
+
+
+
+
